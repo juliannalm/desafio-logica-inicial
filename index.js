4f1d4e4b3ffa73788bbf7db5bd5e5f460e66d5b9
Julianna <141092572+juliannalm@users.noreply.github.com>
2024-01-11 23:05:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,31 @@ pokemons = [pokemon1, pokemon2, pokemon3]
 for pokemon in pokemons:
     mensagem = f"{pokemon['nome']} cadastrado com sucesso!"
     print(mensagem)
+
+
+# Receber nome e experiência do herói
+nome_heroi = input("Digite o nome do herói: ")
+experiencia = int(input("Digite a quantidade de experiência do herói: "))
+
+# Utilizar estrutura de decisão para determinar o nível
+if experiencia < 1000:
+    nivel = "Ferro"
+elif 1001 <= experiencia <= 2000:
+    nivel = "Bronze"
+elif 2001 <= experiencia <= 5000:
+    nivel = "Prata"
+elif 5001 <= experiencia <= 7000:
+    nivel = "Ouro"
+elif 7001 <= experiencia <= 8000:
+    nivel = "Platina"
+elif 8001 <= experiencia <= 9000:
+    nivel = "Ascendente"
+elif 9001 <= experiencia <= 10000:
+    nivel = "Imortal"
+else:
+    nivel = "Radiante"
+
+# Exibir a mensagem de saída
+mensagem_saida = f"O Herói de nome {nome_heroi} está no nível de {nivel}"
+print(mensagem_saida)
+
